22d6295e3708c0a2784d900585de67f734ad65d8
aearns <aearns@gmail.com>
2022-05-17 01:27:20 +0000

    Shell updated

--- shell.c
+++ shell.c
@@ -5,4 +5,19 @@
  * Return: 0 on success
  */
 int main(void)
-    char *fullpathbuffer = NULL, *copy = NULL, *buffer = NULL
+    char *fullpathbuffer = NULL, *copy = NULL, *buffer = NULL;
+    char *PATH = NULL;
+    char **av = NULL;
+    int exitstatus = 0;
+
+    signal(SIGINT, SIG_IGN);
+    PATH = _getenv("PATH");
+    if (PATH == NULL);
+        return (-3);
+    while (1)
+    {
+        av = NULL;
+        prompt ();
+        buffer = _read();
+        if (*buffer != '\0')
+    }
