c63a710d98f7115943d9a28417e88bb2ab92ac56
aearns <aearns@gmail.com>
2022-05-17 01:13:00 +0000

    Header file uploaded

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <signal.h>
+#include <fcntl.h>
+
+extern char **environ;
+
+/* PATH Shell Functions */
+
+/* String Helper*/
+char *_strdup(char *str);
+int _splitstring(char *str);
+
+/* Tokenize and PATH Helper Functions*/
+char _splitPATH(char *str);
+
+/* Program */
+int *_read(void);
